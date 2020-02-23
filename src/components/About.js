78af77fad2bc708014c74a9b80d2710c1ead9187
adiparnes <51050601+adiparnes@users.noreply.github.com>
2020-02-23 15:08:35 +0200

    Initial commit

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow'
+
+const About = ()=>{
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique sed deserunt at rerum consequatur vel sunt ipsa officia accusamus! Maiores similique aliquam non ut architecto libero laborum accusamus quas eum?</p>
+            
+        </div>
+    )
+}
+
+export default Rainbow(About)
